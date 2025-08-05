8313a7bc263512ba2ed12a94e295f8c0431e3c21
Sean van der Merwe <seanvdmerwe2505@gmail.com>
2025-08-05 15:58:12 +0200

    Updated to debug some issues

--- org-hierarchy-app/src/app/api/employees/route.js
+++ org-hierarchy-app/src/app/api/employees/route.js
@@ -1,13 +1,16 @@
 import mysql from 'mysql2/promise';
 
 export async function GET(request) {
+  console.log(`DB_HOST: ${process.env.DB_HOST}`);
+  console.log(`DB_PORT: ${process.env.DB_PORT}`);
+  console.log(`DB_USER: ${process.env.DB_USER}`);
+  console.log(`DB_PASS: ${process.env.DB_PASS}`);
   const db = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
   });
-
   try {
     const [rows] = await db.execute('SELECT * FROM employees');
     return new Response(JSON.stringify(rows), {
